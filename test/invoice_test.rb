22e5fb818fb30277776911889cae151939b71da1
sidewinder2020 <sdwinder1@gmail.com>
2017-07-28 13:01:24 -0600

    reassigned a few tests to inv repo test file from inv test

--- test/invoice_test.rb
+++ test/invoice_test.rb
@@ -37,28 +37,5 @@ class InvoiceTest < Minitest::Test
     assert_equal Time.parse("2014-04-13"), @invoice.created_at
     assert_equal Time.parse("2015-01-20"), @invoice.updated_at
   end
-
-  def test_all_returns_all_invoices_in_the_correct_format
-    assert_equal 4985, @inr.all.count
-  end
-
-  def test_find_by_id
-    assert_instance_of Invoice, @inr.find_by_id(11)
-    assert_nil @inr.find_by_id(1903834)
-  end
-
-  def test_find_all_by_customer_id
-    assert_equal 10, @inr.find_all_by_customer_id(107).count
-    assert_equal [], @inr.find_all_by_customer_id(1238435209)
-  end
-
-  def test_find_all_by_merchant_id
-    assert_equal 12, @inr.find_all_by_merchant_id(12335955).count
-    assert_equal [], @inr.find_all_by_merchant_id(2)
-  end
-
-  def test_find_all_by_status
-    assert_equal 79, @inr.find_all_by_status("pending")
-    # assert_equal [], @inr.find_all_by_status("bonkers")
-  end
+  
 end
