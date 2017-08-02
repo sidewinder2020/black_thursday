edfcef9915b7465437b1c032c57f110f3e39f816
sidewinder2020 <sdwinder1@gmail.com>
2017-08-02 15:09:33 -0600

    formatting our "special offers", not at all suspicious, email

--- lib/sa_modules/sa_bounty_hunter.rb
+++ lib/sa_modules/sa_bounty_hunter.rb
@@ -9,25 +9,24 @@ module SaBountyHunter
   end
 
   def email_body
-    some_string = "Dear Mr. Dog, We here at the Umbrella Corp.
-    \n, I mean Etsy, value our customers. However, while we understand that
-    \n various circumstances may occur, we are unable to compromise in regards
-    \n to our revenue stream. It's for the customers, you see.  If we don't
-    \n have money to build our mansions, I mean pay our workers, we cannot
-    \n possibly continue.  So while it may seem trivial, we believe it to be of
-    \n upmost importance that our policy is 'understood' by all those who
-    \n share in our Etsy Family. We have a few individuals who we would
-    \n like to extend a special offer, along with a reminder of how much Etsy
-    \n values their patronage. We believe you to have a particular set of
-    \n skills, that make you ideally suited to extend these offers, on our
-    behalf.
-    \n Please see below: \n "
+    some_string = "Dear Mr. Dog, \n \n We here at the Umbrella Corp.
+    , I mean Etsy, value our customers. However, while we understand that
+    various circumstances may occur, we are unable to compromise in regards
+    to our revenue stream. \n It's for the customers, you see.  If we don't
+    have money to build our mansions, I mean pay our workers, we cannot
+    possibly continue.  So while it may seem trivial, we believe it to be of
+    upmost importance that our policy is 'understood' by all those who
+    share in our Etsy Family. \n  We have a few individuals who we would
+    like to extend a special offer, along with a reminder of how much Etsy
+    values their patronage. \n  We believe you to have a particular set of
+    skills, that make you ideally suited to extend these offers, on our
+    behalf. \n \n  Please see below: \n "
     email_string
-    another_string = "Please note, for each dear customer that chooses
-    \n to accept our gracious offer, you will receive a 10% portion in
-    \n recognition, of your hard work.  Thank you, and please, don't contact
-    \n us directly, we'll contact you.
-     \n Sincerely, \n The Etsy Family,\n
+    another_string = "\n Please note, for each dear customer that chooses
+    to accept our gracious offer, you will receive a 10% portion in
+    recognition, of your hard work.  Thank you, and please, don't contact
+    us directly, we'll contact you.
+    \n \n Sincerely, \n The Etsy Family,\n
     We meet your catnip pillow needs \n"
     some_string + email_string + another_string
   end
