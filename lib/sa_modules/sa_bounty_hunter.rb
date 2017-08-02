57cdc09e0465bb94c42690e048af50e887df3de4
Brandon Stewart <boveus@gmail.com>
2017-08-02 15:07:09 -0600

    Modify bounty hunter text.

--- lib/sa_modules/sa_bounty_hunter.rb
+++ lib/sa_modules/sa_bounty_hunter.rb
@@ -9,24 +9,25 @@ module SaBountyHunter
   end
 
   def email_body
-    some_string = "Dear Mr. Dog, \n \n We here at the Umbrella Corp.
+    some_string = "Dear Mr. Dog, We here at the Umbrella Corp.
     \n, I mean Etsy, value our customers. However, while we understand that
     \n various circumstances may occur, we are unable to compromise in regards
-    \n to our revenue stream. \n It's for the customers, you see.  If we don't
+    \n to our revenue stream. It's for the customers, you see.  If we don't
     \n have money to build our mansions, I mean pay our workers, we cannot
     \n possibly continue.  So while it may seem trivial, we believe it to be of
     \n upmost importance that our policy is 'understood' by all those who
-    \n share in our Etsy Family. \n  We have a few individuals who we would
+    \n share in our Etsy Family. We have a few individuals who we would
     \n like to extend a special offer, along with a reminder of how much Etsy
-    \n values their patronage. \n  We believe you to have a particular set of
+    \n values their patronage. We believe you to have a particular set of
     \n skills, that make you ideally suited to extend these offers, on our
-    behalf. \n \n  Please see below: \n "
+    behalf.
+    \n Please see below: \n "
     email_string
-    another_string = "\n Please note, for each dear customer that chooses
+    another_string = "Please note, for each dear customer that chooses
     \n to accept our gracious offer, you will receive a 10% portion in
     \n recognition, of your hard work.  Thank you, and please, don't contact
     \n us directly, we'll contact you.
-     \n \n Sincerely, \n The Etsy Family,\n
+     \n Sincerely, \n The Etsy Family,\n
     We meet your catnip pillow needs \n"
     some_string + email_string + another_string
   end
